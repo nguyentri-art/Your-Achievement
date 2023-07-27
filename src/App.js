cb8cffb3f608464ce2cb021e29c2c7fff7ee0880
ngtri <ngtri1410@gmail.com>
2023-07-27 07:34:48 +0700

    set-up

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 import Header from './component/Header';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
+import Main from './component/Main';
+
 
 const App = function() {
   return (
     <div>
+      
       <Header />
       
     </div>
---
+++ src/component/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'Main.css';
+
+const Main = () => {
+    return (
+        <h3>
+            The Main;
+        </h3>
+    );
+}
+
+export default Main;
