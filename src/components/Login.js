b06ce3838ba828c106691cea62c88ab3271fc9f8
ngtri <ngtri1410@gmail.com>
2023-08-07 15:55:32 +0700

    navbar-setup

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,17 +3,76 @@ import "./Login.css";
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { gapi } from 'gapi-script';
 import { useState,useEffect } from 'react';
+import 'dotenv/config';
+
+const clientId = process.env.REACT_APP_GOOGLE_ID_KEY;
+
 
 const Login = () => {
 
-    <div className='container_Navbar'>
-        <image src="public\assert\logo-YAchievement-removebg-preview.png"/>
-        
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: clientId,
+        plugin_name: "chat",
+      });
+    }
+    gapi.load('client:auth2', start);
+  }, []);
+
+  const [loading,setLoading] = useState('Loading.....');
+  const [user, setUser] = useState(null);
+
+  const handleLoginSuccess = (response) => {
+    console.log('Login Success ', response);
+    setUser(response.profileObj);
+    setLoading();
+  }
+
+  const handleLoginFailure = error => {
+    console.log("Login Failure ", error);
+    setLoading();
+  }
+ 
+  const handleLogoutSuccess = (response) => {
+    console.log("Logout Success ", response);
+    setUser(null);
+  }
+ 
+  const handleLogoutFailure = error => {
+    console.log("Logout Failure ", error);
+  }
+ 
+  const handleRequest = () => {
+    setLoading("Loading...");
+  }
+ 
+  const handleAutoLoadFinished = () => {
+    setLoading();
+  }
+
+return (
+    <div classNam="Login_Button">
+        {user ? <div>
+        <div className="name">Welcome {user.name}!</div>
+        <GoogleLogout
+            clientId={clientId}
+            onLogoutSuccess={handleLogoutSuccess}
+            onFailure={handleLogoutFailure}
+        />
+        <pre>{JSON.stringify(user, null, 2)}</pre>
+        </div> : 
+        <GoogleLogin 
+        clientId={clientId}
+        buttonText={loading}
+        onSuccess={handleLoginSuccess}
+        onFailure={handleLoginFailure}
+        onRequest={handleRequest}
+        onAutoLoadFinished={handleAutoLoadFinished}
+        isSignedIn={true} />
+        }
     </div>
-
-
-
-
+    );
 }
 
 export default Login;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Login from './Login';
 
 const Navbar = () => {
 
-    <div className='container_Navbar'>
-        <image src="public\assert\logo-YAchievement-removebg-preview.png"/>
-        <FontAwesomeIcon icon="fa-regular fa-user" />
-        <FontAwesomeIcon icon="fa-solid fa-star" />
+    const logo = require("../logo-YAchievement-removebg-preview.png").default;
 
+    return (
+    <div className='container_Navbar'>
+        <div className='child-1'>
+            <div className='logo_Image'/>
+        </div>
+        <div className="list_Navbar">
+            <h3>YAchievement</h3>
+            <h3>Challenge</h3>
+        </div>
+        <div className="child-2">
+            <Login className="login_button"/>
+        </div>
     </div>
+    );
 }
 
 export default Navbar;
